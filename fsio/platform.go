8daf52b9f211d9a91ec63849a16bed3c21d03fed
YannBcf <Yannbeats.pro@gmail.com>
2024-02-25 10:49:17 +0100

    style: replace tabs with spaces

--- fsio/platform.go
+++ fsio/platform.go
@@ -1,62 +1,62 @@
 package fsio
 
 import (
-	"fmt"
-	"runtime"
-	"strings"
+    "fmt"
+    "runtime"
+    "strings"
 )
 
 type Platform string
 
 const (
-	windowsPlatform Platform = "windows"
-	linuxPlatform   Platform = "linux"
+    windowsPlatform Platform = "windows"
+    linuxPlatform   Platform = "linux"
 )
 
 type platformStruct struct {
-	Windows Platform
-	Linux   Platform
+    Windows Platform
+    Linux   Platform
 }
 
 func (p *platformStruct) Validate(value string) (*Platform, error) {
-	if value != string(windowsPlatform) && value != string(linuxPlatform) {
-		return nil, fmt.Errorf("unsupported platform %s", value)
-	}
+    if value != string(windowsPlatform) && value != string(linuxPlatform) {
+        return nil, fmt.Errorf("unsupported platform %s", value)
+    }
 
-	platform := Platform(value)
-	return &platform, nil
+    platform := Platform(value)
+    return &platform, nil
 }
 
 func ParseArguments(args []string) (*Platform, string, error) {
-	platform := runtime.GOOS
-	out := "binaries"
+    platform := runtime.GOOS
+    out := "binaries"
 
-	l := len(args)
-	for i := 0; i < l; i++ {
-		switch args[i] {
-		case "-o":
-			{
-				if l < i+2 || strings.HasPrefix(args[i+2], "-") {
-					return nil, "", fmt.Errorf("not enough arguments passed for the output")
-				}
+    l := len(args)
+    for i := 0; i < l; i++ {
+        switch args[i] {
+        case "-o":
+            {
+                if l < i+2 || strings.HasPrefix(args[i+2], "-") {
+                    return nil, "", fmt.Errorf("not enough arguments passed for the output")
+                }
 
-				out = args[i+1]
-				break
-			}
+                out = args[i+1]
+                break
+            }
 
-		case "-linux", "-windows":
-			{
-				platform = args[i][1:]
-				break
-			}
-		}
-	}
+        case "-linux", "-windows":
+            {
+                platform = args[i][1:]
+                break
+            }
+        }
+    }
 
-	_platform, err := Platforms.Validate(platform)
-	return _platform, out, err
+    _platform, err := Platforms.Validate(platform)
+    return _platform, out, err
 }
 
 var Platforms = platformStruct{
-	Windows: windowsPlatform,
-	Linux:   linuxPlatform,
+    Windows: windowsPlatform,
+    Linux:   linuxPlatform,
 }
--- github/tags.go
+++ github/tags.go
@@ -1,65 +1,65 @@
 package github
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
 )
 
 type _tagCommit struct {
-	Sha string `json:"sha"`
+    Sha string `json:"sha"`
 }
 
 type _tag struct {
-	Name   string     `json:"name"`
-	Commit _tagCommit `json:"commit"`
+    Name   string     `json:"name"`
+    Commit _tagCommit `json:"commit"`
 }
 
 type Tag struct {
-	Version string
-	Sha     string
+    Version string
+    Sha     string
 }
 
 func fetchRepositoryTags(repo string) ([]_tag, error) {
-	response, err := http.Get(fmt.Sprintf("https://api.github.com/repos/%s/tags", repo))
-	if err != nil {
-		return nil, fmt.Errorf("an error occured while fetching %s repo tags", repo)
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(response.Body)
+    response, err := http.Get(fmt.Sprintf("https://api.github.com/repos/%s/tags", repo))
+    if err != nil {
+        return nil, fmt.Errorf("an error occured while fetching %s repo tags", repo)
+    }
+    defer func(Body io.ReadCloser) {
+        _ = Body.Close()
+    }(response.Body)
 
-	var githubTags []_tag
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
+    var githubTags []_tag
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        return nil, err
+    }
 
-	return githubTags, json.Unmarshal(body, &githubTags)
+    return githubTags, json.Unmarshal(body, &githubTags)
 }
 
 func GetLatestRepositoryTag(repo string) (*Tag, error) {
-	fmt.Println("> Fetching latest version..")
+    fmt.Println("> Fetching latest version..")
 
-	tags, err := fetchRepositoryTags(repo)
-	if err != nil {
-		return nil, err
-	}
+    tags, err := fetchRepositoryTags(repo)
+    if err != nil {
+        return nil, err
+    }
 
-	for _, tag := range tags {
-		if !strings.HasPrefix(tag.Name, "v1.") {
-			continue
-		}
+    for _, tag := range tags {
+        if !strings.HasPrefix(tag.Name, "v1.") {
+            continue
+        }
 
-		if split := strings.Split(tag.Name, "."); len(split) > 0 {
-			return &Tag{
-				Version: split[len(split)-1],
-				Sha:     tag.Commit.Sha,
-			}, nil
-		}
-	}
+        if split := strings.Split(tag.Name, "."); len(split) > 0 {
+            return &Tag{
+                Version: split[len(split)-1],
+                Sha:     tag.Commit.Sha,
+            }, nil
+        }
+    }
 
-	return nil, fmt.Errorf("no tags found for the repo %s", repo)
+    return nil, fmt.Errorf("no tags found for the repo %s", repo)
 }
